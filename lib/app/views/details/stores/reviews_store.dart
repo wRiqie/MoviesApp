78f0bc2d6a7a5edb37322449e69c33ec278396d8
wRiqie <henriquegame512@outlook.com>
2023-01-11 14:02:35 -0300

    adjustments

--- lib/app/views/details/stores/reviews_store.dart
+++ lib/app/views/details/stores/reviews_store.dart
@@ -4,9 +4,12 @@ import 'package:movies_app/app/views/details/states/reviews_state.dart';
 
 class ReviewsStore extends ValueNotifier<ReviewsState> {
   final ReviewRepository _repository;
+  int? movieId;
   ReviewsStore(this._repository) : super(InitialReviewsState());
 
   Future<void> fetchAll(int movieId) async {
+    if(movieId == this.movieId) return;
+    this.movieId = movieId;
     value = LoadingReviewsState();
     final reviews = await _repository.getReviews(movieId);
     value = SuccessReviewsState(reviews);
